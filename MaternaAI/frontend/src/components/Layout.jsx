111808a6367f6a710e3c03ebac25fc570f50e515
Azrin Karim <azrinkarim70@gmail.com>
2026-05-27 12:01:54 +0600

    changed logo

--- MaternaAI/frontend/src/components/Layout.jsx
+++ MaternaAI/frontend/src/components/Layout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import { Menu } from 'lucide-react';
 import { useAuth } from '../context/AuthContext';
+import Logo from './assets/Logo.png'
 
 const Layout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -25,13 +26,9 @@ const Layout = () => {
             >
               <Menu className="w-6 h-6" />
             </button>
-            <div className="flex items-center gap-2">
-              <svg className="w-7 h-7" viewBox="0 0 100 100">
-                <path d="M10,80 C10,30 35,30 40,55 C45,80 55,80 60,55 C65,30 90,30 90,80" stroke="#ab7397" strokeWidth="12" fill="none" strokeLinecap="round" />
-                <circle cx="20" cy="40" r="6" fill="#e1a4c4" />
-                <circle cx="80" cy="40" r="6" fill="#e1a4c4" />
-              </svg>
-              <span className="font-sans font-extrabold text-lg tracking-tight text-text-dark">
+            <div className="flex items-center">
+              <img  className="block -mr-2" src={Logo}  style={{width:"50px", height:"40px",paddingRight:"0px",margin:"-12px"}}/>
+              <span className="font-sans font-extrabold text-lg tracking-tight text-text-dark" style={{paddingRight:"0px"}}>
                 aterna<span className="text-primary-mauve">AI</span>
               </span>
             </div>
