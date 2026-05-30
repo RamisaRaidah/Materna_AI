b7eb4ab22fbb104d600ad229a76b22da3e1c5c20
Ramisa_Raidah <ramisaraidah@gmail.com>
2026-05-30 08:31:44 +0600

    Changes to be committed: 	modified:   MaternaAI/backend/config.py

--- MaternaAI/backend/config.py
+++ MaternaAI/backend/config.py
@@ -13,6 +13,7 @@ CORS_ORIGINS = os.getenv(
 	"http://localhost:5173,http://127.0.0.1:5173,"
 	"http://localhost:5174,http://127.0.0.1:5174,"
 	"http://localhost:3000,http://127.0.0.1:3000",
+    "https://materna-ai-eta.vercel.app",
 ).split(",")
 COHERE_API_KEY = os.getenv("COHERE_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
